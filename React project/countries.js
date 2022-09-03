b16b83c3e9e3598fe0379231a274307343dc7c41
Farzana Akter <67409808+far156@users.noreply.github.com>
2022-09-04 00:00:35 +0600

    Add files via upload

---
+++ React project/countries.js
@@ -0,0 +1,32 @@
+const loadCountries=()=>{
+    fetch("https://restcountries.com/v2/all")
+    .then(res=>res.json())
+    .then(data=>displayCountries(data));
+}
+const displayCountries=countries=>{
+    console.log(countries[0]);    
+    const container=document.getElementById('countries');
+    const countryHTML=countries.map(country=>getCountryHTML(country));
+   container.innerHTML=countryHTML.join('');
+   console.log(countryHTML[0]);
+    
+   
+
+
+    
+
+        
+
+        const getCountryHTML=country=>{
+            const{name,capital,flag}=country;
+            return`
+            <div class="country">
+            <h2>${country.name}<h2/>
+            <h4>Capital:${country.capital}</h4>
+            <img src="${country.flag}">
+            </div>
+            `
+        }
+
+}
+loadCountries();
